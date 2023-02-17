f7b52d3d0fa6f46c6f88d0d866f0c528ee78c8d7
L. Enrique Mangas Martín <405063+mangasmartin@users.noreply.github.com>
2023-02-17 13:17:09 +0100

    Update drapid.js

--- drapid.js
+++ drapid.js
@@ -1,10 +1,10 @@
 const WebSocket = require('ws');
 //const md5 = require('crypto-js/md5');
-const wss = new WebSocket.Server({ port: process.env.PORT || 80 });
+const wss = new WebSocket.Server({ port: process.env.PORT || 3000 });
 let rooms = [];
 
 wss.on("connection", function (ws,req) {
-  //if (req.headers.origin === "https://dinami.cat"){
+  if (req.headers.origin === "https://dinami.cat"){
     //set random num id room
     let room, pass, key, connec = false;
     if (req.url == "/create"){
@@ -65,9 +65,9 @@ wss.on("connection", function (ws,req) {
       }
     });
 
-  //} else {
-    //ws.close();
-  //}
+  } else {
+    ws.close();
+  }
 });
 
 function randomNum(min, max, last){
